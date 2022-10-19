e18a8c2930a4be0f9623ea70a7f79e341f02aeb7
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-10-19 09:24:08 -0100

    added a new file, called RestaurantCards inside the FeaturedRow file

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
 
@@ -9,7 +9,19 @@ const FeaturedRow = ({id,title, description}) => {
         <Text className="font-bold text-lg">{title}</Text>
         <ArrowRightIcon color="#00CCBB"/>
       </View>
+
       <Text className="text-xs text-gray-500 mx-4">{description}</Text>
+
+      <ScrollView
+      horizontal
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+      }}
+      showsHorizontalScrollIndicator={false}
+      className="pt-4"
+      >
+        {/**RestaurantCards */}
+      </ScrollView>
     </View>
   )
 }
