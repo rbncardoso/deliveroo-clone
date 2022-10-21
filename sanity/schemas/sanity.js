3a7185897aa03494ead34a8e6c87ff82f6779114
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-10-21 12:01:24 -0100

    added file sanity.js in schemas and install @sanity/client and @sanity/image-url on the project,and  now goind to deploy

---
+++ sanity/schemas/sanity.js
@@ -0,0 +1,7 @@
+import { SanityClient } from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
+
+const client = sanityClient({
+    projectId : "8ch9srqr"
+
+});
