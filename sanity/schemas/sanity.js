5316214835f845ba2c3b72a0f5a0c745b5d4ed74
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-10-24 15:00:17 -0100

    configurind the sanity.js, const client is done for now

--- sanity/schemas/sanity.js
+++ sanity/schemas/sanity.js
@@ -1,7 +1,10 @@
-import { SanityClient } from "@sanity/client";
+import sanityClient, { SanityClient } from "@sanity/client";
 import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 
 const client = sanityClient({
-    projectId : "8ch9srqr"
+    projectId : "8ch9srqr",
+    dataset: "production",
+    useCdn: "ture",
+    apiVersion: "2022-10-24",
 
 });
