7548063bf6016bb7d268ca639b6b2d7d83cdd033
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-10-25 17:07:13 -0100

    added query Grog into into client.fetch(..) and set the data from backend to be show in frontend..now will change the RestaurantCards field to show them

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,9 +1,34 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
 import RestaurantCards from './RestaurantCards'
+import client from '../sanity'
 
 const FeaturedRow = ({id,title, description}) => {
+  const [restaurants, setRestaurants] = useState([]);
+
+
+  useEffect(()=>{
+
+    client.fetch(
+      `
+      *[_type == "featured" && _id == $id]{
+          ...,
+          restaurants[]->{
+           ...,
+           dishes[]->,
+           type->{
+           name
+          }
+        },
+      }[0]
+    `,
+     {id}
+    ).then((data)=>{
+      setRestaurants(data?.restaurants);
+    });
+  },[]);
+
   return (
     <View>
       <View className="mt-4 flex-row items-center justify-between px-4 ">
