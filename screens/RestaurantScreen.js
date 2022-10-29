293a85b57fd6bcd7f4d734c3e0e5755926d2f2d8
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-10-29 12:23:22 -0100

    Adding dishrows now

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -64,6 +64,18 @@ const RestaurantScreen = () => {
           </View>
           <Text className="text-gray-500 mt-2 pb-4">{shortDescription}</Text>
         </View>
+
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20}/>
+          <Text className="pl-2 flex-1 text-md font-bold">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB"/>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
       </View>
     </ScrollView>
   )
