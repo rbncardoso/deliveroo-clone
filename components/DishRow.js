6b1c6146cdffd121663b8fda6048f26326b54cf5
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-08 17:15:53 -0100

    currency work now after delete package.json.lock and add react-currency-formatter with yarn, also added dish image to DishRow.js

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,5 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import Currency from 'react-currency-formatter';
+import { urlFor } from '../sanity';
 
 const DishRow = ({ id, name, short_description, price, image }) => {
   return (
@@ -7,9 +9,23 @@ const DishRow = ({ id, name, short_description, price, image }) => {
       <View>
         <Text className="text-lg mb-1">{name}</Text>
         <Text className="text-gray-400">{short_description}</Text>
+        <Text className="text-gray-400 mt-2">
+          <Currency quantity={price} currency="CVE" />
+        </Text>
+      </View>
+      <View>
+        <Image 
+          style={{
+            broderWith: 1,
+            borderColor: "#F3F3F4",
+          }}
+          source ={{ uri: urlFor(image).url() }}
+          className="h-20 w-20 bg-gray-300 p-4"
+        />
       </View>
     </TouchableOpacity>
   )
 }
+
 
 export default DishRow
