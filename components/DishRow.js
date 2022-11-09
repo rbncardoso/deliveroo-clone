715ec4165320c1ac6a2ad3324193bf771efc3c6a
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-09 11:14:54 -0100

    added the image in the end of DishRow, adding button to increase and a another one to dicrease

--- components/DishRow.js
+++ components/DishRow.js
@@ -5,24 +5,29 @@ import { urlFor } from '../sanity';
 
 const DishRow = ({ id, name, short_description, price, image }) => {
   return (
-    <TouchableOpacity>
-      <View>
-        <Text className="text-lg mb-1">{name}</Text>
-        <Text className="text-gray-400">{short_description}</Text>
-        <Text className="text-gray-400 mt-2">
-          <Currency quantity={price} currency="CVE" />
-        </Text>
+    <TouchableOpacity className="bg-white border border-gray-200">
+      <View className="flex-row ">
+
+        <View className="flex-1 pl-2 py-4">
+          <Text className="text-lg mb-1">{name}</Text>
+          <Text className="text-gray-400">{short_description}</Text>
+          <Text className="text-gray-400 mt-2 ">
+            <Currency quantity={price} currency="CVE" />
+          </Text>
+        </View>
+
+        <View className="pt-3">
+          <Image
+            style={{
+              broderWith: 1,
+              borderColor: "#F3F3F4",
+            }}
+            source={{ uri: urlFor(image).url() }}
+            className="h-20 w-20 bg-gray-300 p-4"
+          />
+        </View>
       </View>
-      <View>
-        <Image 
-          style={{
-            broderWith: 1,
-            borderColor: "#F3F3F4",
-          }}
-          source ={{ uri: urlFor(image).url() }}
-          className="h-20 w-20 bg-gray-300 p-4"
-        />
-      </View>
+
     </TouchableOpacity>
   )
 }
