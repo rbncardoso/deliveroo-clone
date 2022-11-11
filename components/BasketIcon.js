c068428f87437b519ea098dceac3e78c54ccc0c9
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-11 11:16:57 -0100

    added BasketScreen to the App, when pressed in View Basket showns it

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -13,13 +13,15 @@ const BasketIcon = () => {
 
   return (
     <View className="absolute bottom-10 w-full z-50 px-2">
-     <TouchableOpacity className="bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1"> 
-      <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2 rounded">{items.length}</Text>
-      <Text className="flex-1 text-white font-extrabold text-lg text-center  ">View Basket</Text>
-      <Text className="text-lg  text-white font-extrabold">
-        <Currency quantity={basketTotal} currency="CVE"/>
-      </Text>
-     </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Basket')}
+        className="bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1"
+      >
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2 rounded">{items.length}</Text>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center  ">View Basket</Text>
+        <Text className="text-lg  text-white font-extrabold">
+          <Currency quantity={basketTotal} currency="CVE" />
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
