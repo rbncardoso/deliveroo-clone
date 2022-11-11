8a82dd8be2656189b967849f63a5aabc221144e1
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-11 00:01:02 -0100

    added BasketIcon, showing the number of dishes and total price to pay

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,17 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { selectBasketItems } from '../features/basketSlice'
+import { selectBasketItems, selectBasketTotal } from '../features/basketSlice'
 import { useNavigation } from '@react-navigation/native'
+import Currency from 'react-currency-formatter';
 
 const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
   const navigation = useNavigation();
+  const basketTotal = useSelector(selectBasketTotal);
 
 
   return (
-    <View>
-      <Text>BasketIcon</Text>
+    <View className="absolute bottom-10 w-full z-50 px-2">
+     <TouchableOpacity className="bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1"> 
+      <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2 rounded">{items.length}</Text>
+      <Text className="flex-1 text-white font-extrabold text-lg text-center  ">View Basket</Text>
+      <Text className="text-lg  text-white font-extrabold">
+        <Currency quantity={basketTotal} currency="CVE"/>
+      </Text>
+     </TouchableOpacity>
     </View>
   )
 }
