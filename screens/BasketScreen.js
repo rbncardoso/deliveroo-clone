860667519ec04fa51d940a3b8698c30b413e2fa8
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-11 14:03:56 -0100

    created restaurantSlice.js and its reducer function, passed it to the store.js

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const BasketScreen = () => {
+
   return (
+    <SafeAreaView>
+
     <View>
       <Text>BasketScreen</Text>
     </View>
+    </SafeAreaView>
   )
 }
 
