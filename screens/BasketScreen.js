46cf7403764bca43149e1d4f3693a0e285d807b8
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2022-11-14 11:12:49 -0100

    Added restaurant name on the top of the BasketScreen.js and the XCircleIcon to go back

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectRestaurant } from '../features/restaurantSlice'
 import { selectBasketItems } from '../features/basketSlice'
+import { XCircleIcon } from 'react-native-heroicons/solid'
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -13,12 +14,29 @@ const BasketScreen = () => {
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
   const dispatch = useDispatch();
 
-  return (
-    <SafeAreaView>
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
 
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className=" flex-1 bg-gray-100">
+        <View className="p-5 border-b boder-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="font-bold text-lg text-center">BasketScreen</Text>
+            <Text className="text-sm text-gray-400 text-center">{restaurant.title}</Text>
+          </View>
+          <TouchableOpacity onPress={navigation.goBack}
+            className="absolute top-3 right-5 rounded-full">
+            <XCircleIcon color="#00CCBB" size={50} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
